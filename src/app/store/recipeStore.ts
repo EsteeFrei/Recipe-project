86403db6326df5472f286ed1a8e9dbbeb53db9cd
dinndei <dinn0533@gmail.com>
2024-11-13 12:08:30 +0200

    add filteredRecipes to store, init recipies and filteredRecipies from store

--- src/app/store/recipeStore.ts
+++ src/app/store/recipeStore.ts
@@ -1,21 +1,37 @@
 import { create } from "zustand";
 import { IRecipe } from "../types/recipe";
+import { getRecipes } from "../actions/recipeActions";
 
 interface IRecipeStore {
     recipes: IRecipe[];
+    filteredRecipe: IRecipe[];
     currentRecipe: IRecipe | null;
+    loadRecipes: () => void;
+    setFilteredRecipe: (recipes: IRecipe[]) => void;
     addRecipe: (newRecipe: IRecipe) => void;
-    deleteRecipe: (id:string) => void;
+    deleteRecipe: (id: string) => void;
+
 }
 
 export const useRecipeStore = create<IRecipeStore>((set) => ({
     recipes: [],//get
     currentRecipe: null,
-    addRecipe: (newRecipe: IRecipe) => 
+    filteredRecipe: [],
+    loadRecipes: async () => {
+        const fetchRecipes:IRecipe[]|[] = await getRecipes();
+        set({ 
+            recipes: fetchRecipes,
+            filteredRecipe:fetchRecipes
+         });
+    },
+    setFilteredRecipe: (recipes: IRecipe[]) => {
+        set({filteredRecipe: recipes})
+    },
+    addRecipe: (newRecipe: IRecipe) =>
         set((state) => ({
             recipes: [...state.recipes, newRecipe] // מחזירים את המערך המעודכן
         })),
-    deleteRecipe: (id: string) => 
+    deleteRecipe: (id: string) =>
         set((state) => ({
             recipes: state.recipes.filter(item => item._id !== id) // מחזירים את המערך לאחר הסינון
         }))
